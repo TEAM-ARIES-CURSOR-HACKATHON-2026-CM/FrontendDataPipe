18853404f98aa19abfb4376428cac81ad6b23899
NguetchuissiBrunel <landrybrunel5@gmail.com>
2026-06-03 20:10:55 +0100

    ONBOARDING4

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    optimizeDeps: {
+      include: ['xlsx'],
+    },
     build: {
       rollupOptions: {
         output: {
